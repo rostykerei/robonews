0968d7326ad4603399bf5ebe436359284c06fc1e
Rosty Kerei <rosty.kerei@gmail.com>
2014-02-16 12:06:30 +0100

    major update

--- services/service-text-tools/src/main/java/io/robonews/service/text/tools/MetaPropertyFetcher.java
+++ services/service-text-tools/src/main/java/io/robonews/service/text/tools/MetaPropertyFetcher.java
@@ -6,39 +6,63 @@
  */
 package io.robonews.service.text.tools;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MetaPropertyFetcher {
 
-    private static final Pattern ogImagePattern = Pattern.compile(
-            "<meta[^>]+property\\s*=\\s*[\"|']og:image[\"|'][^>]+content\\s*=\\s*['|\"](.*)['|\"].*>",
+    private static final Pattern metaPropsPattern = Pattern.compile(
+            "<meta[^>]+(name|property|content)\\s*=\\s*[\"']{1}(.+?)[\"']{1}[^>]+(name|property|content)\\s*=\\s*['\"]{1}(.+?)['\"]{1}.*>",
             Pattern.CASE_INSENSITIVE);
 
-    /**
-     * Returns &lt;meta property="og:image"/&gt; content value
-     * or null if not found;
-     *
-     * @param content HTML content
-     * @return value of og:image meta tag
-     */
-    public static String fetchOpenGraphImage(String content) {
-        if (content == null || content == "") {
-            return null;
-        }
+    public static Map<String, String> fetchMetaProps(String content) {
+        Map<String, String> result = new HashMap<String, String>();
 
-        Matcher matcher = ogImagePattern.matcher(content);
+        Matcher matcher = metaPropsPattern.matcher(content);
 
-        if (matcher.find()) {
+        while (matcher.find()) {
 
-            String url = matcher.group(1);
+            String name1 = matcher.group(1);
+            String val1 = matcher.group(2);
 
-            if (url != null && UrlValidator.isValid(url)) {
-                return url;
+            String name2 = matcher.group(3);
+            String val2 = matcher.group(4);
+
+            if (("name".equalsIgnoreCase(name1) || "property".equalsIgnoreCase(name1)) && "content".equalsIgnoreCase(name2)) {
+                result.put(val1.toLowerCase(), val2.trim());
+            }
+            else if (("name".equalsIgnoreCase(name2) || "property".equalsIgnoreCase(name2)) && "content".equalsIgnoreCase(name1)) {
+                result.put(val2.toLowerCase(), val1.trim());
             }
         }
 
-        return null;
+        return result;
+    }
+
+    public static Set<String> fetchMetaImages(String content) {
+
+        Set<String> result = new HashSet<String>();
+
+        Map<String, String> metaProps = fetchMetaProps(content);
+
+        for (Map.Entry<String, String> entry : metaProps.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+
+            if ( "og:image".equals(key) || "thumbnail_url".equals(key) ||
+                    "rnews:thumbnailurl".equals(key) || "twitter:image".equals(key) ) {
+
+                if (!result.contains(value) && UrlValidator.isValid(value)) {
+                    result.add(value);
+                }
+            }
+        }
+
+        return result;
     }
 
 }
--- services/service-text-tools/src/test/java/io/robonews/service/text/tools/MetaPropertyFetcherTest.java
+++ services/service-text-tools/src/test/java/io/robonews/service/text/tools/MetaPropertyFetcherTest.java
@@ -9,91 +9,132 @@ package io.robonews.service.text.tools;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Set;
+
 public class MetaPropertyFetcherTest {
 
     @Test
-    public void testFetchOpenGraphImage() throws Exception {
-        Assert.assertEquals("http://www.robonews.io/",
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n<meta property=\"og:image\" content=\"http://www.robonews.io/\" />\nzzz"
-            )
+    public void testFetchOGImage1() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+            "xxx\n<meta property=\"og:image\" content=\"http://www.robonews.io/\" />\nzzz"
         );
 
-        Assert.assertEquals("http://www.robonews.io/",
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n<meta   property=\"og:image\"    content=\"http://www.robonews.io/\">\nzzz"
-            )
-        );
-
-        Assert.assertEquals("http://www.robonews.io/",
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n<META PrOPErTY=\"OG:ImaGE\" COnTeNT=\"http://www.robonews.io/\" />\nzzz"
-            )
-        );
-
-        Assert.assertEquals("http://www.robonews.io/",
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n<meta property='og:image' content='http://www.robonews.io/'/>\nzzz"
-            )
-        );
-
-        Assert.assertEquals("http://www.robonews.io/",
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n<meta property= \"og:image\" content = 'http://www.robonews.io/'/>\nzzz"
-            )
-        );
-
-        Assert.assertEquals("http://www.robonews.io/",
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n<meta\r\nproperty=\"og:image\"\ncontent=\"http://www.robonews.io/\" />\nzzz"
-            )
-        );
-
-        Assert.assertEquals("http://www.robonews.io/",
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n" +
-                "<meta property=\"og:image\" content=\"http://www.robonews.io/\" />\n" +
-                "yyy\n" +
-                "<meta property=\"og:image\" content=\"http://www.google.com/\" />\n" +
-                "zzz\n"
-            )
-        );
-
-        Assert.assertNull(
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n<meta property=\"og:image\" con tent=\"http://www.robonews.io/\" />\nzzz"
-            )
-        );
-
-        Assert.assertNull(
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n<meta property=\"og:video\" content=\"http://www.robonews.io/\" />\nzzz"
-            )
-        );
-
-        Assert.assertNull(
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n<meta property=\"og:image\" content=\"not valid url\" />\nzzz"
-            )
-        );
-
-        Assert.assertNull(
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                "xxx\n<no-meta property=\"og:image\" content=\"http://www.robonews.io/\" />\nzzz"
-            )
-        );
-
-        Assert.assertNull(
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                null
-            )
-        );
-
-        Assert.assertNull(
-            MetaPropertyFetcher.fetchOpenGraphImage(
-                ""
-            )
-        );
-
+        Assert.assertTrue(urls.contains("http://www.robonews.io/"));
     }
+
+    @Test
+    public void testFetchOGImage2() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+                "xxx\n<meta    content=\"http://www.robonews.io/\"    property=\"og:image\">\nzzz"
+        );
+
+        Assert.assertTrue(urls.contains("http://www.robonews.io/"));
+    }
+
+    @Test
+    public void testFetchOGImage3() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+                "xxx\n<META PrOPErTY='OG:ImaGE' COnTeNT=\"http://www.robonews.io/\" />\nzzz"
+        );
+
+        Assert.assertTrue(urls.contains("http://www.robonews.io/"));
+    }
+
+    @Test
+    public void testFetchOGImage4() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+                "xxx\n<meta content='http://www.robonews.io/' property='thumbnail_url'/>\nzzz"
+        );
+
+        Assert.assertTrue(urls.contains("http://www.robonews.io/"));
+    }
+
+    @Test
+    public void testFetchOGImage5() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+                "xxx\n<meta property= \"twitter:image\" content = 'http://www.robonews.io/'/>\nzzz"
+        );
+
+        Assert.assertTrue(urls.contains("http://www.robonews.io/"));
+    }
+
+    @Test
+    public void testFetchOGImage6() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+                "xxx\n<meta\r\nproperty=\"og:image\"\ncontent=\"http://www.robonews.io/\" />\nzzz"
+        );
+
+        Assert.assertTrue(urls.contains("http://www.robonews.io/"));
+    }
+
+    @Test
+    public void testFetchOGImage7() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+                "xxx\n" +
+                        "<meta property=\"og:image\" content=\"http://www.google.com/\" />\n" +
+                        "yyy\n" +
+                        "<meta content=\"http://www.robonews.io/\" property=\"rnews:thumbnailurl\"/>\n" +
+                        "zzz\n"
+        );
+
+        Assert.assertTrue(urls.contains("http://www.google.com/"));
+        Assert.assertTrue(urls.contains("http://www.robonews.io/"));
+    }
+
+    @Test
+    public void testFetchOGImage7b() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+                "xxx\n" +
+                        "<meta property=\"og:image\" content=\"http://www.robonews.io/\" />\n" +
+                        "yyy\n" +
+                        "<meta content=\"http://www.robonews.io/\" property=\"rnews:thumbnailurl\"/>\n" +
+                        "zzz\n"
+        );
+
+        Assert.assertTrue(urls.contains("http://www.robonews.io/"));
+        Assert.assertEquals(1, urls.size());
+    }
+
+    @Test
+    public void testFetchOGImage8() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+            "xxx\n<meta property=\"og:image\" con tent=\"http://www.robonews.io/\" />\nzzz"
+        );
+
+        Assert.assertEquals(0, urls.size());
+    }
+
+    @Test
+    public void testFetchOGImage9() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+                "xxx\n<meta property=\"og:video\" content=\"http://www.robonews.io/\" />\nzzz"
+        );
+
+        Assert.assertEquals(0, urls.size());
+    }
+
+    @Test
+    public void testFetchOGImage10() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+                "xxx\n<meta property=\"og:image\" content=\"not valid url\" />\nzzz"
+        );
+
+        Assert.assertEquals(0, urls.size());
+    }
+
+    @Test
+    public void testFetchOGImage11() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages(
+            "xxx\n<no-meta property=\"og:image\" content=\"http://www.robonews.io/\" />\nzzz"
+        );
+
+        Assert.assertEquals(0, urls.size());
+    }
+
+    @Test
+    public void testFetchOGImage12() throws Exception {
+        Set<String> urls = MetaPropertyFetcher.fetchMetaImages("");
+        Assert.assertEquals(0, urls.size());
+    }
+
 }
