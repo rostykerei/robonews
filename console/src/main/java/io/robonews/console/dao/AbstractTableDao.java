b2e1efeaa65db028976a2d565977e44777fb01a4
Rosty Kerei <rosty.kerei@gmail.com>
2014-10-31 15:52:36 +0100

    general upadte

--- console/src/main/java/io/robonews/console/dao/AbstractTableDao.java
+++ console/src/main/java/io/robonews/console/dao/AbstractTableDao.java
@@ -1,5 +1,49 @@
+/**
+ * Robonews.io
+ *
+ * Copyright (c) 2013-2014 Rosty Kerei.
+ * All rights reserved.
+ */
 package io.robonews.console.dao;
 
-public abstract class AbstractTableDao {
+import io.robonews.console.datatable.Datatable;
+import io.robonews.console.datatable.DatatableCriteria;
+import io.robonews.dao.AbstractDao;
+import java.io.Serializable;
+import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
+
+public abstract class AbstractTableDao<T, E> {
+
+    protected abstract AbstractDao<T, ? extends Serializable> getDao();
+
+    protected abstract String[] getSearchFields();
+
+    protected abstract E mapFromOriginal(T obj);
+
+    @Transactional(readOnly = true)
+    public Datatable<E> getDatatable(DatatableCriteria criteria) {
+
+        Datatable<E> datatable = new Datatable<>();
+
+        datatable.setDraw(criteria.getDraw());
+        datatable.setRecordsTotal(getDao().getCountAll());
+        datatable.setRecordsFiltered(getDao().getTableCount(criteria.getSearch(), getSearchFields()));
+
+        List<T> list = getDao().getTable(
+            criteria.getStart(),
+            criteria.getLength(),
+            criteria.getSortField(),
+            criteria.getSortDirection() == DatatableCriteria.SortDirection.ASC,
+            criteria.getSearch(),
+            getSearchFields()
+        );
+
+        for (T channel : list) {
+            datatable.getData().add(mapFromOriginal(channel));
+        }
+
+        return datatable;
+    }
 
 }
--- console/src/main/java/io/robonews/console/dao/ChannelConsoleDao.java
+++ console/src/main/java/io/robonews/console/dao/ChannelConsoleDao.java
@@ -1,48 +1,39 @@
+/**
+ * Robonews.io
+ *
+ * Copyright (c) 2013-2014 Rosty Kerei.
+ * All rights reserved.
+ */
 package io.robonews.console.dao;
 
-import io.robonews.console.datatable.Datatable;
-import io.robonews.console.datatable.DatatableCriteria;
 import io.robonews.console.dto.ChannelDatatableItem;
+import io.robonews.dao.AbstractDao;
 import io.robonews.dao.ChannelDao;
 import io.robonews.domain.Channel;
-import java.util.List;
+import java.io.Serializable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
-public class ChannelConsoleDao extends AbstractTableDao {
+public class ChannelConsoleDao extends AbstractTableDao<Channel, ChannelDatatableItem> {
+
+    private static final String[] SEARCH_FIELDS = new String[] { "name", "url" };
 
     @Autowired
     private ChannelDao channelDao;
 
-    private String[] SEARCH_FIELDS = new String[] {"name", "url"};
+    @Override
+    protected AbstractDao<Channel, ? extends Serializable> getDao() {
+        return channelDao;
+    }
 
-    @Transactional(readOnly = true)
-    public Datatable<ChannelDatatableItem> getDatatable(DatatableCriteria criteria) {
+    @Override
+    protected String[] getSearchFields() {
+        return SEARCH_FIELDS;
+    }
 
-        Datatable<ChannelDatatableItem> datatable = new Datatable<ChannelDatatableItem>();
-
-        datatable.setDraw(criteria.getDraw());
-        datatable.setRecordsTotal(channelDao.getCountAll());
-        datatable.setRecordsFiltered(channelDao.getTableCount(criteria.getSearch(), SEARCH_FIELDS));
-
-        List<Channel> list = channelDao.getTable(
-            criteria.getStart(),
-            criteria.getLength(),
-            criteria.getSortField(),
-            criteria.getSortDirection() == DatatableCriteria.SortDirection.ASC,
-            criteria.getSearch(),
-            SEARCH_FIELDS
-        );
-
-        for (Channel channel : list) {
-            datatable.getData().add(
-                ChannelDatatableItem.fromChannel(channel)
-            );
-        }
-
-        return datatable;
-
+    @Override
+    protected ChannelDatatableItem mapFromOriginal(Channel obj) {
+        return ChannelDatatableItem.fromChannel(obj);
     }
 }
